a7a54343f457550dabee972b5cd2e644540ecc52
ALOUA Hajar <118297724+itsZORAL1@users.noreply.github.com>
2024-09-27 14:23:46 +0000

    working on responsive breakpoints

--- maalem_web/src/pages/onboarding.tsx
+++ maalem_web/src/pages/onboarding.tsx
@@ -1,20 +1,20 @@
-
 import React from 'react';
-import Footer from '@/components/layout/onboardingScreenFooter'
+import Footer from '@/components/layout/onboardingScreenFooter';
 import NavBar from '@/components/layout/onboardingScreenHeader';
 
 const OnboardingPage: React.FC = () => {
   return (
-    <div className=" bg-black   bg-cover bg-center bg-no-repeat flex flex-col min-h-screen   w-full ">
-   
-     <NavBar />
-      <main className="flex flex-col justify-end flex-grow w-full   ">
-      <h1 className="text-8xl text-left  ml-[10%] text-white ">Need a Hand?</h1>
-      <h1 className="text-4xl text-customGreen text-center font-black  ml-[520px]" >We've Got You Covered!</h1>
+    <div className="bg-black bg-cover bg-center bg-no-repeat flex flex-col min-h-screen w-full">
+      <NavBar />
+      <main className="flex flex-col justify-end flex-grow w-full">
+        <h1 className="text-4xl md:text-7xl sm:text-6xl  lg:text-8xl text-left ml-0 lg:ml-[150px] sm:ml-[120px]  md:ml-[120px] text-white">
+          Need a Hand?
+        </h1>
+        <h1 className="text-3xl lg:text-4xl md:text-4xl sm:text-2xl  text-customGreen text-center font-black ml-0 lg:ml-[400px] sm:ml-[270px] md:ml-[310px] ">
+          We've Got You Covered!
+        </h1>
       </main>
-
-    <Footer/>
-
+      <Footer />
     </div>
   );
 };
