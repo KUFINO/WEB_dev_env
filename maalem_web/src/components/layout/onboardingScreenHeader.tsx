5a8806c6023ec3e920ea9179eb4e9754685bdb91
ALOUA Hajar <118297724+itsZORAL1@users.noreply.github.com>
2024-09-28 18:01:22 +0000

    adding new color palette

--- maalem_web/src/components/layout/onboardingScreenHeader.tsx
+++ maalem_web/src/components/layout/onboardingScreenHeader.tsx
@@ -9,22 +9,22 @@ return (
     <div className="container mx-auto flex justify-between items-center ">
 
     <ul className="flex space-x-4" >
-        <li><a href="/" className="p-2 text-white hover:text-customGreen text-sm  " >Maalems</a></li>
-        <li><a href="/about" className=" p-2 text-white hover:text-customGreen text-sm">Features</a></li>
-        <li><a href="/contact" className="  p-2 text-white hover:text-customGreen text-sm">Pricing</a></li>
-        <li><a href="/contact" className=" p-2 text-white hover:text-customGreen text-sm">Ressources</a></li>
+        <li><a href="/" className="p-2 text-white hover:text-phoenix text-sm  " >Maalems</a></li>
+        <li><a href="/about" className=" p-2 text-white hover:text-phoenix text-sm">Features</a></li>
+        <li><a href="/contact" className="  p-2 text-white hover:text-phoenix text-sm">Pricing</a></li>
+        <li><a href="/contact" className=" p-2 text-white hover:text-phoenix text-sm">Ressources</a></li>
     </ul>
 
-    <h1 className="text-2xl text-customGreen text-center font-black" >MaaleM</h1>
+    <h1 className="text-2xl text-phoenix text-center font-black" >MaaleM</h1>
 
     <ul className="flex space-x-4">
     <SearchbarPage/>
     <Link href="/loginpage">
-    <button className="animate-bounce bg-transparent h-10 border  border-customGreen rounded-mixed text-sm py-2 px-4 ">
+    <button className="animate-bounce bg-transparent h-10 border  border-phoenix rounded-mixed text-sm py-2 px-4 ">
         Log in
       </button>
     </Link>
-    <button className=" bg-customGreen  h-10  text-sm  py-2 px-4  rounded-15p" >
+    <button className=" bg-phoenix  h-10  text-sm  py-2 px-4  rounded-15p" >
        Get Started
     </button>
     </ul>
--- maalem_web/src/pages/onboarding.tsx
+++ maalem_web/src/pages/onboarding.tsx
@@ -10,7 +10,7 @@ const OnboardingPage: React.FC = () => {
         <h1 className="text-4xl md:text-7xl sm:text-6xl  lg:text-8xl text-left ml-0 lg:ml-[150px] sm:ml-[120px]  md:ml-[120px] text-white">
           Need a Hand?
         </h1>
-        <h1 className="text-3xl lg:text-4xl md:text-4xl sm:text-2xl  text-customGreen text-center font-black ml-0 lg:ml-[400px] sm:ml-[270px] md:ml-[310px] ">
+        <h1 className="text-3xl lg:text-4xl md:text-4xl sm:text-2xl  text-phoenix text-center font-black ml-0 lg:ml-[400px] sm:ml-[270px] md:ml-[310px] ">
           We&apos; ve Got You Covered!
         </h1>
       </main>
