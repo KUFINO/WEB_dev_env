a623bf591ac0ca63002a0bb10e79e8c8bdd52144
ALOUA Hajar <118297724+itsZORAL1@users.noreply.github.com>
2024-10-04 21:42:20 +0000

    fixing styling layout

--- maalem_web/src/app/login/components/AuthButtons.tsx
+++ maalem_web/src/app/login/components/AuthButtons.tsx
@@ -3,17 +3,17 @@ import { Button } from "@/components/ui/button";
 import { GoogleIcon, GitHubIcon } from "../../../../public/assets/icons/Icons";
 
 const AuthButtons: React.FC = () => (
-  <div className="lg:mt-4 flex space-x-4 justify-center">
+  <div className="flex justify-center space-x-4 sm:mt-4 md:mt-4 lg:mt-4">
     <Button
       variant="outline"
-      className="flex items-center justify-center w-32 sm:w-36 md:w-40 h-10 py-3 px-4 sm:px-5 md:px-6 bg-transparent text-dobby font-bold"
+      className="flex items-center justify-center w-32 h-10 py-3 px-4 bg-transparent text-dobby font-bold sm:w-36 sm:px-5 md:w-40 md:px-6"
     >
       <GoogleIcon className="w-5 h-5 mr-2" />
       <span className="font-bold">Google</span>
     </Button>
     <Button
       variant="outline"
-      className="flex items-center justify-center w-32 sm:w-36 md:w-40 h-10 py-3 px-4 sm:px-5 md:px-6 bg-transparent text-dobby font-bold"
+      className="flex items-center justify-center w-32 h-10 py-3 px-4 bg-transparent text-dobby font-bold sm:w-36 sm:px-5 md:w-40 md:px-6"
     >
       <GitHubIcon className="w-5 h-5 mr-2" />
       <span className="font-bold">GitHub</span>
