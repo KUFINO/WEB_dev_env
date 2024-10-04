33d2f21d3fe80929952e14cd45175574792f7d5d
grainme <marouanebouf3@gmail.com>
2024-10-05 00:16:31 +0100

    quick merge fix

--- maalem_web/src/components/ui/searchbar.tsx
+++ maalem_web/src/components/ui/searchbar.tsx
@@ -1,11 +1,10 @@
 import React, { FC } from 'react';
 import { Input } from "@/components/ui/input";
 
-<<<<<<< HEAD
 const SearchbarPage : React.FC=()=>{
 return (
-    <div className='max-w-md mx-auto  ' >
-    <div className="relative flex items-center w-full h-9 rounded-lg overflow-hidden border border-white rounded-custom " >
+    <div className='max-w-md mx-auto'>
+      <div className="relative flex items-center w-full h-9 rounded-lg overflow-hidden border border-white rounded-custom " >
         <div className="grid place-items-center h-full w-9 text-gray-300   ">
             <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor " >
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
@@ -17,10 +16,10 @@ return (
         type="text"
         id="search"
         placeholder="Find a Creator" /> 
-    </div>
-</div>
-);
-=======
+      </div>
+    </div>);
+}
+
 const SearchIcon: FC<React.SVGProps<SVGSVGElement>> = (props) => {
   return (
     <svg
@@ -39,7 +38,6 @@ const SearchIcon: FC<React.SVGProps<SVGSVGElement>> = (props) => {
       <path d="m21 21-4.3-4.3" />
     </svg>
   );
->>>>>>> main
 };
 
 const SearchComponent: FC = () => {
